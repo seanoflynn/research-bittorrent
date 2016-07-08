50dcf5daa9e4978003a74bc87e10702e734a1b9e
Justin Van Patten <jvp@justinvp.com>
2016-07-08 16:47:54 -0700

    Avoid array allocation in MemoryStreamExtensions.Append
    The temporary byte[] array allocation can be avoided by using
    MemoryStream.WriteByte.

--- BitTorrent/BEncoding.cs
+++ BitTorrent/BEncoding.cs
@@ -284,7 +284,7 @@ namespace BitTorrent
     {
         public static void Append(this MemoryStream stream, byte value)
         {
-            stream.Append(new[] { value });
+            stream.WriteByte(value);
         }
 
         public static void Append(this MemoryStream stream, byte[] values)
